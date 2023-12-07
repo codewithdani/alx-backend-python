04848e7d0f9ea3c0228e0076c8957c59633291c8
Daniel <daniel@gmail.com>
2023-12-07 18:21:56 +0300

    100-safe_first_element.py

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task 10 module.
+"""
+from typing import Union, Any, Sequence
+
+
+def safe_first_element(lst: List[Union[Any, None]]) -> Sequence[Any]:
+    """
+    This function retrieves the first element of a sequence,
+    safely handling empty list.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None

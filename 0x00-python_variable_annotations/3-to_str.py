f108b3f8c24e1244b9ae876ad2cd88d12370b6e3
Daniel <daniel@gmail.com>
2023-12-07 11:38:47 +0300

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 3's module.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function takes a float
+    and returns its string representation.
+    """
+    return str(n)

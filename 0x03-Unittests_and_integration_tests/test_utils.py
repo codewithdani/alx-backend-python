180f5be8934d3a6ab0b82b9766f6d8d56e319f25
Daniel <daniel@gmail.com>
2024-01-06 16:06:32 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,6 +28,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
+
     def test_access_nested_map_exception(
             self,
             nested_map,
